bc4cfade58ce9c3602e74ffc38ce2c9dc2907ee6
João Cardoso <pedrogc.joao@gmail.com>
2026-05-18 21:09:58 -0300

    Inicialização Activity principal

--- app/src/main/java/br/ufpr/conversormoeda/Controller/MainActivity.kt
+++ app/src/main/java/br/ufpr/conversormoeda/Controller/MainActivity.kt
@@ -1,11 +1,15 @@
 package br.ufpr.conversormoeda.Controller
 
+
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.ufpr.conversormoeda.R
+import br.ufpr.conversormoeda.model.Wallet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +21,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var reaisView: TextView = findViewById<TextView>(R.id.reaisView)
+        var dolaresView: TextView = findViewById<TextView>(R.id.dolaresView)
+        var bitcoinsView: TextView = findViewById<TextView>(R.id.bitcoinsView)
+
+        var wallet: Wallet = Wallet()
+
+        reaisView.text = DecimalFormat("#0.00").format(wallet.reais)
+        dolaresView.text = DecimalFormat("#0.00").format(wallet.dolares)
+        bitcoinsView.text = DecimalFormat("#0.000000").format(wallet.bitcoins)
+
     }
 }
--- app/src/main/java/br/ufpr/conversormoeda/model/Wallet.kt
+++ app/src/main/java/br/ufpr/conversormoeda/model/Wallet.kt
@@ -1,4 +1,7 @@
 package br.ufpr.conversormoeda.model
 
 class Wallet {
+    var reais:  Double = 100000.0;
+    var dolares: Double = 50000.0;
+    var bitcoins: Double = 0.5;
 }
